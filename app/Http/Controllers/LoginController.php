ce1cc0790f09389f050f8a4f137160fd9295fcc8
Dhruvish <dsorathiya@patoliyainfotech.com>
2023-11-30 18:46:39 +0530

    code formate change

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -29,41 +29,41 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            if(Auth::check()){
-                return redirect()->intended('users/dashboard');
-            }
+            return redirect()->intended('users/dashboard');
         }
         return back()->with([
             'fail' => 'The provided credentials do not match our records.',
         ]);
 
     }
-    public function dashboard(){
+    public function dashboard()
+    {
         return view('users/dashboard');
     }
 
-    public function verify(Request $request,$id){
-            event(new Verified($request->user()));
-            $time             = Carbon::now();
-            $timeStamp        = $time->toDateTimeString();
-            $inputArray = array(
-                'email_verified_at' => $timeStamp,
-            );
-            // dd($inputArray);
-            if($inputArray ){
-                $userServices = new UserServices();
-                $query = $userServices->update($id,$inputArray);
-                if($query){
-                    // dd($query);
-                    return redirect('/');
-                }else{
-                    dd();
-                    return redirect('/');
-                }
-            }else{
+    public function verify(Request $request, $id)
+    {
+        event(new Verified($request->user()));
+        $time = Carbon::now();
+        $timeStamp = $time->toDateTimeString();
+        $inputArray = array(
+            'email_verified_at' => $timeStamp,
+        );
+        // dd($inputArray);
+        if ($inputArray) {
+            $userServices = new UserServices();
+            $query = $userServices->update($id, $inputArray);
+            if ($query) {
+                // dd($query);
+                return redirect('/');
+            } else {
                 dd();
                 return redirect('/');
             }
+        } else {
+            dd();
+            return redirect('/');
+        }
 
 
 

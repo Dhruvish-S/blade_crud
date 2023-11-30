beb613aabbee4fa0283ab9cacda848f1c8a06904
Dhruvish <dsorathiya@patoliyainfotech.com>
2023-11-30 16:16:09 +0530

    new line added

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -87,10 +87,6 @@
         </table>
     </div>
 
-
-
-    {{-- <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
     <script>
         $(document).ready(function() {
--- resources/views/users/welcomeemail.blade.php
+++ resources/views/users/welcomeemail.blade.php
@@ -4,12 +4,15 @@
 </head>
 <body>
     <div class="d-flex justify-content-center align-items-center  w-100" style="height:800px;">
-        <div class="bg-light box" class="bg-secondary" style="width: 500px; height: 500px;  box-shadow: 10px 10px;">
+        <div class="bg-light box" class="bg-secondary" style="width: 500px; height: 500px;  box-shadow: 5px 4px 2px 3px;">
         <h4 style="text-align: center;padding: 20px;color: blue; font-size: 30px;font-weight:800">Laravel Crud</h4>
-            <p style="text-align: center;font-weight:800;font-size:22px;color:blue">Welcome to the User Registration form</p>
+            <p style="text-align: center;font-weight:800;font-size:22px;color:blue">Welcome to the User Registration</p>
                 <p style="text-align: center;font-weight:800;font-size:22px;color:blue">{{ $first_name }}</p><br><br>
-        <a href="{{ url('verify/' . $id) }}" style="font-weight:800;font-size:16px;color:white;background-color:black;padding:10px;border-radius:5px; margin-left:35%">Verify Email{{ $id }}</a>
+        {{-- <a href="{{ url('verify/' . $id) }}" style="font-weight:800;font-size:16px;color:white;background-color:black;padding:10px;border-radius:5px; margin-left:35%">Verify Email{{ $id }}</a> --}}
+        <div class="button" style="display:flex;align-items: center;justify-content: center;">
+            <a href="{{ url('verify/' . $id) }}" style="font-weight:800;font-size:16px;color:white;background-color:black;padding:10px;border-radius:5px;">Verify Email</a>
         </div>
+    </div>
     </div>
 </body>
 </html>

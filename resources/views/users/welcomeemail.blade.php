08877398a4aa28635e2f6c4953656ab2ef6924dd
Dhruvish <dsorathiya@patoliyainfotech.com>
2023-12-02 10:02:34 +0530

    add new code

--- resources/views/users/welcomeemail.blade.php
+++ resources/views/users/welcomeemail.blade.php
@@ -8,7 +8,6 @@
         <h4 style="text-align: center;padding: 20px;color: blue; font-size: 30px;font-weight:800">Email Verification</h4>
             <p style="text-align: center;font-weight:800;font-size:16px;color:blue">Thanks for signup with us,click on the button below to <br/>verify your email address.</p>
                 <p style="text-align: center;font-weight:800;font-size:22px;color:blue">{{ $first_name }} {{ $last_name }}</p><br><br>
-        {{-- <a href="{{ url('verify/' . $id) }}" style="font-weight:800;font-size:16px;color:white;background-color:black;padding:10px;border-radius:5px; margin-left:35%">Verify Email{{ $id }}</a> --}}
         <div class="button" style="display:flex;align-items: center;justify-content: center;">
             <a href="{{ url('verify/' . $id) }}" style="font-weight:800;font-size:16px;color:white;background-color:black;padding:10px;border-radius:5px;">Verify Email</a>
         </div>
